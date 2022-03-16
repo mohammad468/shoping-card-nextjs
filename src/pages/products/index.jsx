5d10e96ce714b50120b70b9e84f6c7411067e8ff
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-16 19:17:36 +0330

    add products route

---
+++ src/pages/products/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Store from "../../components/Store";
+import MyNav from "../../components/MyNav";
+
+const index = () => {
+  return (
+    <>
+      <MyNav />
+      <Store />
+    </>
+  );
+};
+
+export default index;
