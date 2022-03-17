fec3035e5a6a223884ec07f60d2ead80bd0d5b3f
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-17 13:55:23 +0330

    electronics page

--- src/pages/products/electronics/index.jsx
+++ src/pages/products/electronics/index.jsx
@@ -1,35 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import MyElectronicsCards from "../../../components/electronics/MyElectronicsCards";
+import MyCard from "../../../components/MyCard";
 import MyNav from "../../../components/MyNav";
 import { fetchProducts } from "../../../redux/products/productsAction";
 
 const index = () => {
-  const dispatch = useDispatch();
-  const productsState = useSelector((state) => state.productsState);
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
-
-  let electronicsProducts = [];
-
-  productsState.products.map((product) => {
-    if (product.category == "electronics") {
-      electronicsProducts.push(product);
-    }
-  });
-
-  console.log(electronicsProducts);
-
   return (
-    <div>
+    <>
       <MyNav />
-      {electronicsProducts ? (
-        electronicsProducts.map((product) => <h1>{product.title}</h1>)
-      ) : (
-        <h1>Loading...</h1>
-      )}
-    </div>
+      <MyElectronicsCards />
+    </>
   );
 };
 
