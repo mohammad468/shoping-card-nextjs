68c696342d22690acfb4e35048f9188c7cc18243
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-22 13:44:41 +0430

    RadioGroup

--- src/components/MyDropdown.jsx
+++ src/components/MyDropdown.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
 import Link from "next/Link";
 import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+import { RadioGroup } from "@headlessui/react";
+
+const plans = [
+  {
+    name: "Startup",
+    ram: "12GB",
+    cpus: "6 CPUs",
+    disk: "160 GB SSD disk",
+  },
+  {
+    name: "Business",
+    ram: "16GB",
+    cpus: "8 CPUs",
+    disk: "512 GB SSD disk",
+  },
+  {
+    name: "Enterprise",
+    ram: "32GB",
+    cpus: "12 CPUs",
+    disk: "1024 GB SSD disk",
+  },
+];
 
 const MyDropdown = () => {
   const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState(plans[0]);
 
   const showHandler = () => {
     if (!show) {
@@ -20,60 +43,84 @@ const MyDropdown = () => {
   };
 
   return (
-    <div
-      className="bg-gray-100 h-screen flex flex-col justify-center items-center"
-      onClick={hiddenHandler}
-    >
-      <div className="relative">
-        <button
-          className={
-            show
-              ? "bg-blue-700 text-white text-lg px-4 py-3 rounded-xl ring-2 ring-blue-400 ring-offset-2 ring-offset-white"
-              : "bg-blue-500 text-white text-lg px-4 py-3 rounded-xl hover:bg-blue-600 transition-all"
-          }
-          onClick={showHandler}
-        >
-          <span>Show more</span>
-          {show ? (
-            <AiOutlineCaretDown className="inline ms-2" />
-          ) : (
-            <AiOutlineCaretUp className="inline ms-2" />
-          )}
-        </button>
-        <div
-          className={`${
-            show ? "" : "hidden"
-          } bg-white w-56 mt-2 rounded-xl overflow-hidden border-1 border-gray-200 absolute`}
-        >
-          <Link href="#">
-            <a className="block hover:text-gray-700 text-gray-700 p-2 hover:bg-gray-100">
-              Link 1
-            </a>
-          </Link>
-          <Link href="#">
-            <a className="block hover:text-gray-700 text-gray-700 p-2 hover:bg-gray-100">
-              Link 2
-            </a>
-          </Link>
-          <Link href="#">
-            <a className="block hover:text-gray-700 text-gray-700 p-2 hover:bg-gray-100">
-              Link 3
-            </a>
-          </Link>
-          <Link href="#">
-            <a className="block hover:text-gray-700 text-gray-700 p-2 hover:bg-gray-100">
-              Link 4
-            </a>
-          </Link>
-          <Link href="#">
-            <a className="block hover:text-gray-700 text-gray-700 p-2 hover:bg-gray-100">
-              Link 5
-            </a>
-          </Link>
-        </div>
+    <div className="w-full px-4 py-16">
+      <div className="w-full max-w-md mx-auto">
+        <RadioGroup value={selected} onChange={setSelected}>
+          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+          <div className="space-y-2">
+            {plans.map((plan) => (
+              <RadioGroup.Option
+                key={plan.name}
+                value={plan}
+                className={({ active, checked }) =>
+                  `${
+                    active
+                      ? "ring-2 ring-offset-2 ring-offset-sky-300 ring-white ring-opacity-60"
+                      : ""
+                  }
+                  ${
+                    checked ? "bg-sky-900 bg-opacity-75 text-white" : "bg-white"
+                  }
+                    relative rounded-lg shadow-md px-5 py-4 cursor-pointer flex focus:outline-none`
+                }
+              >
+                {({ active, checked }) => (
+                  <>
+                    <div className="flex items-center justify-between w-full">
+                      <div className="flex items-center">
+                        <div className="text-sm">
+                          <RadioGroup.Label
+                            as="p"
+                            className={`font-medium  ${
+                              checked ? "text-white" : "text-gray-900"
+                            }`}
+                          >
+                            {plan.name}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description
+                            as="span"
+                            className={`inline ${
+                              checked ? "text-sky-100" : "text-gray-500"
+                            }`}
+                          >
+                            <span>
+                              {plan.ram}/{plan.cpus}
+                            </span>{" "}
+                            <span aria-hidden="true">&middot;</span>{" "}
+                            <span>{plan.disk}</span>
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                      {checked && (
+                        <div className="flex-shrink-0 text-white">
+                          <CheckIcon className="w-6 h-6" />
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
+              </RadioGroup.Option>
+            ))}
+          </div>
+        </RadioGroup>
       </div>
     </div>
   );
 };
+
+function CheckIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
 
 export default MyDropdown;
