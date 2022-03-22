3769a664081425a68aadfc87119e808e3864b81d
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-22 15:54:42 +0430

    Disclosure

--- src/components/MyDropdown.jsx
+++ src/components/MyDropdown.jsx
@@ -3,124 +3,87 @@ import Link from "next/Link";
 import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
 import { RadioGroup } from "@headlessui/react";
 
-const plans = [
-  {
-    name: "Startup",
-    ram: "12GB",
-    cpus: "6 CPUs",
-    disk: "160 GB SSD disk",
-  },
-  {
-    name: "Business",
-    ram: "16GB",
-    cpus: "8 CPUs",
-    disk: "512 GB SSD disk",
-  },
-  {
-    name: "Enterprise",
-    ram: "32GB",
-    cpus: "12 CPUs",
-    disk: "1024 GB SSD disk",
-  },
-];
-
 const MyDropdown = () => {
   const [show, setShow] = useState(false);
-  const [selected, setSelected] = useState(plans[0]);
+  const [selected, setSelected] = useState(0);
 
-  const showHandler = () => {
+  const showHandler = (id) => {
     if (!show) {
       setShow(true);
+      setSelected(id);
     } else {
       setShow(false);
+      setSelected(0);
     }
   };
 
   const hiddenHandler = () => {
     if (show) {
       setShow(false);
+      setSelected(0);
     }
   };
 
   return (
-    <div className="w-full px-4 py-16">
-      <div className="w-full max-w-md mx-auto">
-        <RadioGroup value={selected} onChange={setSelected}>
-          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
-          <div className="space-y-2">
-            {plans.map((plan) => (
-              <RadioGroup.Option
-                key={plan.name}
-                value={plan}
-                className={({ active, checked }) =>
-                  `${
-                    active
-                      ? "ring-2 ring-offset-2 ring-offset-sky-300 ring-white ring-opacity-60"
-                      : ""
-                  }
-                  ${
-                    checked ? "bg-sky-900 bg-opacity-75 text-white" : "bg-white"
-                  }
-                    relative rounded-lg shadow-md px-5 py-4 cursor-pointer flex focus:outline-none`
-                }
-              >
-                {({ active, checked }) => (
-                  <>
-                    <div className="flex items-center justify-between w-full">
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${
-                              checked ? "text-white" : "text-gray-900"
-                            }`}
-                          >
-                            {plan.name}
-                          </RadioGroup.Label>
-                          <RadioGroup.Description
-                            as="span"
-                            className={`inline ${
-                              checked ? "text-sky-100" : "text-gray-500"
-                            }`}
-                          >
-                            <span>
-                              {plan.ram}/{plan.cpus}
-                            </span>{" "}
-                            <span aria-hidden="true">&middot;</span>{" "}
-                            <span>{plan.disk}</span>
-                          </RadioGroup.Description>
-                        </div>
-                      </div>
-                      {checked && (
-                        <div className="flex-shrink-0 text-white">
-                          <CheckIcon className="w-6 h-6" />
-                        </div>
-                      )}
-                    </div>
-                  </>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
+    <div className="flex justify-center items-start h-screen bg-gray-200">
+      <div className="bg-white rounded-xl max-w-md w-full p-2 mt-3">
+        <div
+          className={`w-full flex items-center justify-between rounded-lg p-2 my-2 cursor-pointer ${
+            show && selected == 1
+              ? "ring-1 ring-purple-400 bg-purple-200 text-blue-700"
+              : "bg-purple-100 hover:bg-purple-200 text-blue-700"
+          }`}
+          onClick={() => showHandler(1)}
+        >
+          <h1 className="text-lg font-bold">What is your refund policy?</h1>
+          {/* {show && selected == 1 ? (
+            <AiOutlineCaretUp className="transition-all duration-700" />
+          ) : (
+            <AiOutlineCaretDown className="transition-all duration-700" />
+          )} */}
+          <AiOutlineCaretDown
+            className={`${
+              show && selected == 1
+                ? "transition-all duration-150"
+                : "rotate-180 transition-all duration-150"
+            }`}
+          />
+        </div>
+        <div
+          className={`text-black opacity-50 py-3 px-2 ${
+            show && selected == 1 ? "" : "hidden"
+          }`}
+        >
+          If you're unhappy with your purchase for any reason, email us within
+          90 days and we'll refund you in full, no questions asked.
+        </div>
+        <div
+          onClick={() => showHandler(2)}
+          className={`w-full flex items-center justify-between rounded-lg p-2 my-2 cursor-pointer ${
+            show && selected == 2
+              ? "ring-1 ring-purple-400 bg-purple-200 text-blue-700"
+              : "bg-purple-100 hover:bg-purple-200 text-blue-700"
+          }`}
+        >
+          <h1 className="text-lg font-bold">Do you offer technical support?</h1>
+          <AiOutlineCaretDown
+            className={`${
+              show && selected == 2
+                ? "transition-all duration-150"
+                : "rotate-180 transition-all duration-150"
+            }`}
+          />
+        </div>
+        <div
+          className={`text-black opacity-50 py-3 px-2 ${
+            show && selected == 2 ? "transition-all" : "hidden"
+          }`}
+        >
+          No.
+        </div>
       </div>
     </div>
   );
 };
-
-function CheckIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
-      <path
-        d="M7 13l3 3 7-7"
-        stroke="#fff"
-        strokeWidth={1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
 
 export default MyDropdown;
