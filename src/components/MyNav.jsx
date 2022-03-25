1c8dd4badcfe58d503af592947682bc1861ffe76
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-25 17:09:35 +0430

    fix bug in import next link

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import { Badge } from "react-bootstrap";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
